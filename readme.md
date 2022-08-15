87cd4ae380ec8bda5c3fed1f8bf6af2bcccdbc01
favoursyre <favourforchrist91@gmail.com>
2022-08-15 18:35:56 +0100

    commit

---
+++ readme.md
@@ -0,0 +1,37 @@
+# Content Placeholder
+
+![content](https://drive.google.com/uc?export=download&id=1MBbwxPkEyO26BEPUdywubBhsIKLrPOjT)
+
+## Table of Contents
+
+1. [Overview](#overview)
+2. [Languages](#languages)
+3. [Technologies](#technologies)
+4. [Installations](#installations)
+5. [Run](#run)
+
+## Overview
+
+A simple script that holds content
+
+## Languages
+
+- HTML
+- CSS
+- Javascript
+
+## Technologies
+
+- Custom JS
+
+## Installations
+
+```bash
+git clone https://github.com/favoursyre/content-placeholder.git && cd content-placeholder
+```
+
+## Run
+
+```bash
+start ./index.html
+```
---
+++ script.js
@@ -0,0 +1,27 @@
+//I want to create a script that would handle the content placeholder
+
+//Declaring the various variables
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+//This gets the data
+function getData() {
+  header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
+  title.innerHTML = 'Software Engineer'
+  excerpt.innerHTML = 'Software engineering is so sweet'
+  profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
+  name.innerHTML = 'Favour Syre'
+  date.innerHTML = 'Apr 26, 1999'
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
